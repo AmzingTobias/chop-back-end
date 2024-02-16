6070d608d2a91622694dac3fafa88d6ff789cb27
Tobias <40802800+AmzingTobias@users.noreply.github.com>
2024-02-16 16:45:17 +0000

    API changes for updating a discount code

--- models/discount.models.ts
+++ models/discount.models.ts
@@ -16,6 +16,23 @@ export type TDiscountCodeValidation = {
   stackable: boolean;
 };
 
+export type TDiscountCodeEntry = {
+  // The id of the discount code
+  id: number;
+  // The code that was validated
+  code: string;
+  // The date the code was created
+  createdOn: Date;
+  // The number of uses left on the code
+  remainingUses: number;
+  // If the discount code is currently valid
+  active: boolean;
+  // The amount the discount code applies to the total order
+  percent: number;
+  // If the discount can be used in conjunction with other offers
+  stackable: boolean;
+};
+
 /**
  * Validate that a discount code exists
  * @param code The code to validate
@@ -42,6 +59,156 @@ export const validateDiscountCode = (
           reject(err);
         } else {
           resolve(res.rowCount > 0 ? res.rows[0] : null);
+        }
+      }
+    );
+  });
+};
+
+/**
+ * Get a list of all the discount codes available
+ * @returns A list of the discount codes
+ */
+export const getAllDiscountCodes = (): Promise<TDiscountCodeEntry[]> => {
+  return new Promise((resolve, reject) => {
+    pool.query(
+      `
+    SELECT 
+      id,
+      code,
+      created_on AS "createdOn",
+      number_of_uses AS "remainingUses",
+      active, 
+      percent_off AS "percent", 
+      stackable 
+    FROM discount_codes
+    `,
+      (err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(res.rows);
+        }
+      }
+    );
+  });
+};
+
+/**
+ * Toggle a discount code being active
+ * @param codeId The id of the code
+ * @param active True if the code is to be made active, false otherwise
+ * @returns EDatabaseResponses.OK if the code was updated,
+ * EDatabaseResponses.DOES_NOT_EXIST If the code does not exist to update
+ */
+export const toggleDiscountCodeActive = (
+  codeId: number,
+  active: boolean
+): Promise<EDatabaseResponses> => {
+  return new Promise((resolve, reject) => {
+    pool.query(
+      "UPDATE discount_codes SET active = $1 WHERE id = $2",
+      [active, codeId],
+      (err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(
+            res.rowCount > 0
+              ? EDatabaseResponses.OK
+              : EDatabaseResponses.DOES_NOT_EXIST
+          );
+        }
+      }
+    );
+  });
+};
+
+/**
+ * Toggle a discount code being stackable
+ * @param codeId The id of the code
+ * @param stackable True if the code is stackable with other codes, false otherwise
+ * @returns EDatabaseResponses.OK if the code was updated,
+ * EDatabaseResponses.DOES_NOT_EXIST If the code does not exist to update
+ */
+export const toggleDiscountCodeStackable = (
+  codeId: number,
+  stackable: boolean
+): Promise<EDatabaseResponses> => {
+  return new Promise((resolve, reject) => {
+    pool.query(
+      "UPDATE discount_codes SET stackable = $1 WHERE id = $2",
+      [stackable, codeId],
+      (err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(
+            res.rowCount > 0
+              ? EDatabaseResponses.OK
+              : EDatabaseResponses.DOES_NOT_EXIST
+          );
+        }
+      }
+    );
+  });
+};
+
+/**
+ * Set a new percentage value for a discount code
+ * @param codeId The id of the code
+ * @param percent The new percent value for the code
+ * @returns EDatabaseResponses.OK if the code was updated,
+ * EDatabaseResponses.DOES_NOT_EXIST If the code does not exist to update
+ */
+export const updateDiscountCodePercent = (
+  codeId: number,
+  percent: number
+): Promise<EDatabaseResponses> => {
+  return new Promise((resolve, reject) => {
+    pool.query(
+      "UPDATE discount_codes SET percent_off = $1 WHERE id = $2",
+      [percent, codeId],
+      (err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(
+            res.rowCount > 0
+              ? EDatabaseResponses.OK
+              : EDatabaseResponses.DOES_NOT_EXIST
+          );
+        }
+      }
+    );
+  });
+};
+
+/**
+ * Set the number of remaining uses for a code
+ * @param codeId The id of the code
+ * @param remainingUses The number of remaining uses left for a code. -1 if the code is
+ * infinite
+ * @returns EDatabaseResponses.OK if the code was updated,
+ * EDatabaseResponses.DOES_NOT_EXIST If the code does not exist to update
+ */
+export const updateDiscountCodeRemainingUses = (
+  codeId: number,
+  remainingUses: number
+): Promise<EDatabaseResponses> => {
+  return new Promise((resolve, reject) => {
+    pool.query(
+      "UPDATE discount_codes SET number_of_uses = $1 WHERE id = $2",
+      [remainingUses, codeId],
+      (err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(
+            res.rowCount > 0
+              ? EDatabaseResponses.OK
+              : EDatabaseResponses.DOES_NOT_EXIST
+          );
         }
       }
     );
--- routes/v1/discounts.routes.ts
+++ routes/v1/discounts.routes.ts
@@ -4,9 +4,72 @@ import {
   EResponseStatusCodes,
   ETextResponse,
 } from "../../common/response-types";
-import { validateDiscountCode } from "../../models/discount.models";
+import {
+  getAllDiscountCodes,
+  toggleDiscountCodeActive,
+  toggleDiscountCodeStackable,
+  updateDiscountCodePercent,
+  updateDiscountCodeRemainingUses,
+  validateDiscountCode,
+} from "../../models/discount.models";
 
 export const discountRouter = Router();
+
+/**
+ * @swagger
+ * /discounts/:
+ *   get:
+ *     tags: [Discounts]
+ *     summary: Get a list of discount codes
+ *     responses:
+ *       200:
+ *         description: List of discount codes
+ *         schema:
+ *           type: array
+ *           items:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: integer
+ *                 description: The id of the code.
+ *               name:
+ *                 type: string
+ *                 code: The code.
+ *               createdOn:
+ *                 type: Date
+ *                 description: The date the code was created
+ *               remainingUses:
+ *                 type: number
+ *                 description: The number of uses left on the code
+ *               active:
+ *                 type: boolean
+ *                 description: If the code is currently active
+ *               percent:
+ *                 type: number
+ *                 description: The percentage value of the code
+ *               stackable:
+ *                 type: boolean
+ *                 description: If the code is stackable with other codes
+ *       401:
+ *          description: Account lacks required permissions
+ *       500:
+ *          description: Internal server error
+ */
+discountRouter.get("/", verifyToken, (req, res) => {
+  if (
+    !req.user ||
+    (req.user.accountType !== EAccountTypes.admin &&
+      req.user.accountType !== EAccountTypes.sales)
+  ) {
+    return res.sendStatus(EResponseStatusCodes.UNAUTHORIZED_CODE);
+  }
+  getAllDiscountCodes()
+    .then((codes) => res.json(codes))
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(EResponseStatusCodes.INTERNAL_SERVER_ERROR_CODE);
+    });
+});
 
 /**
  * @swagger
@@ -69,6 +132,112 @@ discountRouter.get("/code", verifyToken, (req, res) => {
       console.error(err);
       res.sendStatus(EResponseStatusCodes.INTERNAL_SERVER_ERROR_CODE);
     });
+});
+
+/**
+ * @swagger
+ * /discounts/code/{id}:
+ *   get:
+ *     tags: [Discounts]
+ *     summary: Update a discount code
+ *     parameters:
+ *       - in: params
+ *         name: id
+ *         required: true
+ *         description: The id of the code to edit
+ *         schema:
+ *           type: integer
+ *       - in: body
+ *         name: percent
+ *         required: false
+ *         description: The new percentage value for the code
+ *         schema:
+ *           type: integer
+ *       - in: body
+ *         name: active
+ *         required: false
+ *         description: The new active value for the code
+ *         schema:
+ *           type: boolean
+ *       - in: body
+ *         name: stackable
+ *         required: false
+ *         description: The new stackable value for the code
+ *         schema:
+ *           type: boolean
+ *       - in: body
+ *         name: remaining
+ *         required: false
+ *         description: The new remaining uses value for the code
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *          description: The code was updated
+ *       400:
+ *          description: Code id invalid
+ *       401:
+ *          description: Account lacks required permissions
+ *       500:
+ *          description: Internal server error
+ */
+discountRouter.put("/code/:id", verifyToken, async (req, res) => {
+  if (
+    !req.user ||
+    (req.user.accountType !== EAccountTypes.admin &&
+      req.user.accountType !== EAccountTypes.sales)
+  ) {
+    return res.sendStatus(EResponseStatusCodes.UNAUTHORIZED_CODE);
+  }
+
+  const { id } = req.params;
+  if (Number.isNaN(Number(id))) {
+    return res.sendStatus(EResponseStatusCodes.BAD_REQUEST_CODE);
+  }
+
+  const discountCodeId = Number(id);
+  const { percent, stackable, active, remainingUses } = req.body;
+  try {
+    if (typeof percent === "number") {
+      if (percent >= 0 && percent <= 100) {
+        await updateDiscountCodePercent(discountCodeId, percent);
+      }
+    }
+  } catch (err) {
+    console.error(err);
+    return res.sendStatus(EResponseStatusCodes.INTERNAL_SERVER_ERROR_CODE);
+  }
+
+  try {
+    if (typeof stackable === "boolean") {
+      await toggleDiscountCodeStackable(discountCodeId, stackable);
+    }
+  } catch (err) {
+    console.error(err);
+    return res.sendStatus(EResponseStatusCodes.INTERNAL_SERVER_ERROR_CODE);
+  }
+
+  try {
+    if (typeof active === "boolean") {
+      await toggleDiscountCodeActive(discountCodeId, active);
+    }
+  } catch (err) {
+    console.error(err);
+    return res.sendStatus(EResponseStatusCodes.INTERNAL_SERVER_ERROR_CODE);
+  }
+
+  try {
+    if (typeof remainingUses === "number") {
+      if (remainingUses >= -1) {
+        await updateDiscountCodeRemainingUses(discountCodeId, remainingUses);
+      }
+    }
+  } catch (err) {
+    console.error(err);
+    return res.sendStatus(EResponseStatusCodes.INTERNAL_SERVER_ERROR_CODE);
+  }
+
+  return res.sendStatus(200);
 });
 
 export default discountRouter;
