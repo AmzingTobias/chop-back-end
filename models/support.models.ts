c970f37afa883c1b1f504ede63f5a38dca26aa7b
Tobias <40802800+AmzingTobias@users.noreply.github.com>
2024-03-02 19:38:31 +0000

    API for creating support ticket

---
+++ models/support.models.ts
@@ -0,0 +1,39 @@
+import { FOREIGN_KEY_VIOLATION } from "../common/postgresql-error-codes";
+import pool, { EDatabaseResponses, ICustomError } from "../data/data";
+
+/**
+ * Create a new support ticket
+ * @param ticketTitle The title of the ticket
+ * @param customerId The id of the customer who created the ticket
+ * @returns EDatabaseResponses.OK if the ticket was created succesfully,
+ * EDatabaseResponses.FOREIGN_KEY_VIOLATION if the customer Id does not exist
+ * EDatabaseResponses.DOES_NOT_EXIST if the ticket was not inserted for another reason
+ */
+export const createNewSupportTicket = (
+  ticketTitle: string,
+  customerId: number
+): Promise<{ status: EDatabaseResponses; ticketId?: number }> => {
+  return new Promise((resolve, reject) => {
+    pool.query(
+      `
+      INSERT INTO support_tickets(customer_id, title) VALUES ($1, $2) RETURNING id
+    `,
+      [customerId, ticketTitle],
+      (err: ICustomError, res) => {
+        if (err) {
+          if (err.code === FOREIGN_KEY_VIOLATION) {
+            return resolve({
+              status: EDatabaseResponses.FOREIGN_KEY_VIOLATION,
+            });
+          }
+          return reject(err);
+        }
+        return resolve(
+          res.rowCount > 0
+            ? { status: EDatabaseResponses.OK, ticketId: res.rows[0].id }
+            : { status: EDatabaseResponses.DOES_NOT_EXIST }
+        );
+      }
+    );
+  });
+};
--- routes/v1/support.routes.ts
+++ routes/v1/support.routes.ts
@@ -1,5 +1,64 @@
 import { Router } from "express";
+import { EAccountTypes, verifyToken } from "../../security/security";
+import { EResponseStatusCodes } from "../../common/response-types";
+import { createNewSupportTicket } from "../../models/support.models";
+import { EDatabaseResponses } from "../../data/data";
 
 export const supportRouter = Router();
+
+/**
+ * @swagger
+ * /support:
+ *   post:
+ *     tags: [Support]
+ *     summary: Create a new support ticket
+ *     parameters:
+ *       - in: body
+ *         name: title
+ *         required: true
+ *         description: The main contents of the ticket
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Support ticket created
+ *       400:
+ *         description: Request parameters invalid or not supplied
+ *       401:
+ *         description: Account is not a customer
+ *       500:
+ *          description: Internal server error
+ */
+supportRouter.post("/", verifyToken, (req, res) => {
+  // Must be customer co create a new support ticket
+  if (!req.user || req.user.accountType !== EAccountTypes.customer) {
+    return res.sendStatus(EResponseStatusCodes.UNAUTHORIZED_CODE);
+  }
+  const customerId = req.user.accountTypeId;
+  // Get the ticket information from request body
+  const { title: ticketTitle } = req.body;
+  if (typeof ticketTitle !== "string" || ticketTitle.trim().length === 0) {
+    return res.sendStatus(EResponseStatusCodes.BAD_REQUEST_CODE);
+  } else {
+    const trimmedTicketTitle = ticketTitle.trim();
+    createNewSupportTicket(trimmedTicketTitle, customerId)
+      .then((databaseResponse) => {
+        switch (databaseResponse.status) {
+          case EDatabaseResponses.OK:
+            return res.json({ ticketId: databaseResponse.ticketId });
+          case EDatabaseResponses.FOREIGN_KEY_VIOLATION:
+            return res.sendStatus(EResponseStatusCodes.BAD_REQUEST_CODE);
+          default:
+            return res.sendStatus(
+              EResponseStatusCodes.INTERNAL_SERVER_ERROR_CODE
+            );
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        return res.sendStatus(EResponseStatusCodes.INTERNAL_SERVER_ERROR_CODE);
+      });
+  }
+});
 
 export default supportRouter;
